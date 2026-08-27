115ab3b26f8fe802e560a856a55de49a6ff42d13
vyzz-xz <fizdstore@gmail.com>
2026-08-27 10:17:03 +0700

    Praktikum 1: Routing sederhana

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,9 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('hello'); 
 });
+
+use App\Http\Controllers\DashboardController;
+
+Route::get('/dashboard', [DashboardController::class, 'index'])
+    ->middleware(['auth'])
+    ->name('dashboard');

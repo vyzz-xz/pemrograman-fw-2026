99d61bfadca0783cd4cb24eb1af21cd61b208885
vyzz-xz <fizdstore@gmail.com>
2026-08-27 11:00:24 +0700

    Tugas Praktikum 1

--- routes/web.php
+++ routes/web.php
@@ -11,3 +11,10 @@ use App\Http\Controllers\DashboardController;
 Route::get('/dashboard', [DashboardController::class, 'index'])
     ->middleware(['auth'])
     ->name('dashboard');
+
+Route::get('/about', function () {
+    return '<h1>Profil Toko</h1>
+    <p>Barokah Mart adalah sistem kasir digital yang dirancang untuk mempercepat 
+    dan mempermudah transaksi harian toko Anda.</p>
+    <p>Kami melayani dengan jujur dan barokah.</p>';
+});

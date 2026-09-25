e72b53c67e51ee9f822d3fdfd542877f38917125
vyzz-xz <fizdstore@gmail.com>
2026-09-25 14:47:34 +0700

    feat: complete practice 6 eloquent models, relationships, and report

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -17,4 +17,9 @@ class Product extends Model
     {
         return $this->hasMany(TransactionDetail::class);
     }
+
+    public function getPriceRupiahAttribute()
+    {
+        return 'Rp ' . number_format($this->price, 0, ',', '.');
+    }
 }
